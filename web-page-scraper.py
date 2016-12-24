a91c2754e0d51918462bb102abaacb802a7eddd6
Mauro <mauro.bonfietti@gmail.com>
2016-12-24 16:42:31 -0300

    Retoques en nombres de variables y al imprimir la pantalla.

--- web-page-scraper.py
+++ web-page-scraper.py
@@ -1,33 +1,38 @@
 #!/usr/bin/python
-    
+
 from urllib2 import urlopen
 from HTMLParser import HTMLParser
 from collections import defaultdict
 
-o = defaultdict(int)
-d = "http://ordergroove.com/company"
-h = urlopen(d).read()
+tags = defaultdict(int)
+domain = "http://ordergroove.com/company"
+html = urlopen(domain).read()
 
-class P(HTMLParser):
+class Parser(HTMLParser):
     def handle_starttag(self, tag, attrs):
-        o[tag] += 1
+        tags[tag] += 1
 
-parser = P()
-parser.feed(h)
+parser = Parser()
+parser.feed(html)
 
 i = 0
 total = 0
-s = sorted(o, key=o.get, reverse=True)
+sortedtags = sorted(tags, key=tags.get, reverse=True)
 
-for t in s:
-  total += o[t]
+for tag in sortedtags:
+  total += tags[tag]
 
-print 'TOTAL HTML ELEMENTS:', total
+print '[TINY WEB PAGE SCRAPER]'
+print
+print 'TARGET DOMAIN ==>', domain
+print
+print 'TOP 5 HTML ELEMENTS MOST USED:'
 
-print 'TOP 5 HTML ELEMENTS:'
-
-for t in s:
-  print t, o[t]
+for tag in sortedtags:
+  print tag, '==>', tags[tag]
   i += 1
   if i == 5:
       break
+
+print
+print 'HTML TOTAL ==>', total, 'ELEMENTS.'
