690d812cea21682c497d053e760d8c9660471b20
Mauro <mauro.bonfietti@gmail.com>
2016-12-25 23:23:25 -0300

    Tiny Web Page Scraper ;-)

--- web-page-scraper_1.py
+++ web-page-scraper_1.py
@@ -4,26 +4,28 @@ from urllib2 import urlopen
 from HTMLParser import HTMLParser
 from collections import defaultdict
 
-class Parser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        tags[tag] += 1
-
-i = 0
-total = 0
 tags = defaultdict(int)
 domain = "http://ordergroove.com/company"
 html = urlopen(domain).read()
 
+class Parser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        tags[tag] += 1
+
 parser = Parser()
 parser.feed(html)
 
+i = 0
+total = 0
 sortedtags = sorted(tags, key=tags.get, reverse=True)
 
 for tag in sortedtags:
   total += tags[tag]
 
 print '[TINY WEB PAGE SCRAPER]'
+print
 print 'TARGET DOMAIN ==>', domain
+print
 print 'TOP 5 HTML ELEMENTS MOST USED:'
 
 for tag in sortedtags:
@@ -32,4 +34,5 @@ for tag in sortedtags:
   if i == 5:
       break
 
+print
 print 'HTML TOTAL ==>', total, 'ELEMENTS.'
