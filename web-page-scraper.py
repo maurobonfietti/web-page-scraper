8942744964ae8bb5f90a0ff4afec625c91d03db1
Mauro <mauro.bonfietti@gmail.com>
2016-12-26 21:05:46 -0300

    Retoque en Web Page Scraper.

--- web-page-scraper.py
+++ web-page-scraper.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-from urllib2 import urlopen
 from HTMLParser import HTMLParser
 from collections import defaultdict
+from urllib2 import urlopen
 
 class Parser(HTMLParser):
     def handle_starttag(self, tag, attrs):
@@ -12,12 +12,12 @@ i = 0
 total = 0
 tags = defaultdict(int)
 domain = "http://ordergroove.com/company"
-html = urlopen(domain).read()
 
+html = urlopen(domain).read()
 parser = Parser()
 parser.feed(html)
 
-print ("Target Domain: '%s'.\n" % domain)
+print "Target Domain: '%s'.\n" % domain
 print 'Html Tags Most Used:'
 
 for tag in sorted(tags, key=tags.get, reverse=True):
@@ -26,4 +26,4 @@ for tag in sorted(tags, key=tags.get, reverse=True):
     if i <= 5:
         print ('#%d: %s %s' % (i, tag, tags[tag]))
 
-print '\nTotal:', total, 'Html Elements.'
+print '\nTotal:', total, 'Html Elements.\n'
