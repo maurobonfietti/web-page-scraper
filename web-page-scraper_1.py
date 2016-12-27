86b22dff9be2994e0d410ecbedbebb9cc5c6419c
Mauro <mauro.bonfietti@gmail.com>
2016-12-26 23:41:13 -0300

    Refactor Web Page Scraper.

--- web-page-scraper_1.py
+++ web-page-scraper_1.py
@@ -6,34 +6,32 @@ from time import time
 from urllib2 import urlopen
 
 class Parser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.start = time()
+        self.domain = ""
+        self.i = 0
+        self.total = 0
+        self.tags = defaultdict(int)
     def handle_starttag(self, tag, attrs):
-        tags[tag] += 1
+        self.tags[tag] += 1
+    def run(self):
+        print '\n[Tiny Web Page Scraper]\n'
+        self.domain = raw_input("Enter a target domain, for example: http://ordergroove.com/company ==> ")
+        if not self.domain:
+            self.domain = "http://ordergroove.com/company"
+        self.start = time()
+        print "\nTarget Domain:", self.domain
+        html = urlopen(self.domain).read()
+        parser.feed(html)
+        print '\nHtml Tags Most Used:'
+        for tag in sorted(self.tags, key=self.tags.get, reverse=True):
+            self.total += self.tags[tag]
+            self.i += 1
+            if self.i <= 5:
+                print ('#%d: %s %s' % (self.i, tag, self.tags[tag]))
+        print '\nTotal: %d Html Elements.' % self.total
+        print "\nScraped in '%.3f' seconds.\n" % (time()-self.start)
 
-i = 0
-total = 0
-tags = defaultdict(int)
-
-print '\n[Tiny Web Page Scraper]\n'
-
-domain = raw_input("Enter a target domain, for example: http://ordergroove.com/company ==> ")
-if not domain:
-    domain = "http://ordergroove.com/company"
-
-start = time()
-
-print "\nTarget Domain:", domain
-
-html = urlopen(domain).read()
 parser = Parser()
-parser.feed(html)
-
-print '\nHtml Tags Most Used:'
-
-for tag in sorted(tags, key=tags.get, reverse=True):
-    total += tags[tag]
-    i += 1
-    if i <= 5:
-        print ('#%d: %s %s' % (i, tag, tags[tag]))
-
-print '\nTotal: %d Html Elements.' % total
-print "\nScraped in '%.3f' seconds.\n" % (time()-start)
+parser.run()
